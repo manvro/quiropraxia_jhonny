705ec0b3060058b36c4c7b8cb0ff99f05576e04e
Manvro <manvrogame@gmail.com>
2025-07-28 09:15:18 -0400

    Fix tailwind config for production display

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,24 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: ["./src/**/*.{astro,html,js}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
-
-
-// tailwind.config.js
-module.exports = {
+    // tailwind.config.js
+    /** @type {import('tailwindcss').Config} */
+    export default {
     content: ["./src/**/*.{astro,html,js,ts}"],
     theme: {
         extend: {
-            animation: {
-                'fade-down': 'fadeDown 0.8s ease-out',
-                'fade-up': 'fadeUp 0.8s ease-out',
+        animation: {
+            'fade-down': 'fadeDown 0.8s ease-out',
+            'fade-up': 'fadeUp 0.8s ease-out',
+        },
+        keyframes: {
+            fadeDown: {
+            '0%': { opacity: 0, transform: 'translateY(-20px)' },
+            '100%': { opacity: 1, transform: 'translateY(0)' },
             },
-            keyframes: {
-                fadeDown: {
-                    '0%': { opacity: 0, transform: 'translateY(-20px)' },
-                    '100%': { opacity: 1, transform: 'translateY(0)' },
-                },
-                fadeUp: {
-                    '0%': { opacity: 0, transform: 'translateY(20px)' },
-                    '100%': { opacity: 1, transform: 'translateY(0)' },
-                },
+            fadeUp: {
+            '0%': { opacity: 0, transform: 'translateY(20px)' },
+            '100%': { opacity: 1, transform: 'translateY(0)' },
             },
+        },
         },
     },
     plugins: [],
-}
+    }
